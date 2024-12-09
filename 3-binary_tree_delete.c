ca9b2680032ccf226f00345980adfe62273ec7f2
Coco29dev <10059@holbertonstudents.com>
2024-12-09 12:52:48 +0000

    code fonctionnel task 3 mais pas betty

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete an entiere binary tree
+ * @tree: pointer on the tree to delete
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+return;
+	if (tree->left != NULL)
+{
+	binary_tree_delete(tree->left);
+}
+	if (tree->right != NULL)
+{
+	binary_tree_delete(tree->right);
+}
+	free(tree);
+}
