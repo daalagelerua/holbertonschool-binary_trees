021a9bc452fdcb13cfedd8969b0919062d8c0433
Coco29dev <10059@holbertonstudents.com>
2024-12-10 08:39:23 +0000

    code fonctionnel task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t height_left = 0;
-size_t  height_right = 0;
-        if (tree == NULL)
-return (0);
-        height_left = binary_tree_height(tree->left);
-        height_right = binary_tree_height(tree->right);
-return ((height_left > height_right ? height_left : height_right) + 1);
+	if (!tree || (!tree->left && !tree->right))
+	return (0);
+return (max(binary_tree_height(tree->left),
+	binary_tree_height(tree->right)) + 1);
+}
+
+/**
+ * max - returns the max of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: max of a and b
+ */
+
+size_t max(size_t a, size_t b)
+{
+	if (a > b)
+	return (a);
+return (b);
 }
