27e81700c7ac3dcea8ff176f0d740d1c8fc3b208
Coco29dev <10059@holbertonstudents.com>
2024-12-11 07:58:43 +0000

    test checker task7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,10 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	return;
-	if (tree != NULL)
-{
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
-}
 }
