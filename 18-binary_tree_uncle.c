5d0c22d489681614a80bf27cdb44d9e679f25ff5
daalagelerua <aurele284@gmail.com>
2024-12-11 09:04:23 +0000

    function uncle adding comments

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,20 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Finds the uncle of a node
- *                     in a binary tree.
- * @node: A pointer to the node to find the uncle of.
- *
- * Return: If node is NULL or has no uncle, NULL.
- *         Otherwise, a pointer to the uncle node.
- */
+* binary_tree_uncle - Finds the uncle of a node in a binary tree.
+* @node: A pointer to the node to find the uncle of.
+* Return: If node is NULL or has no uncle, NULL.
+*         Otherwise, a pointer to the uncle node.
+*/
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent || !node->parent->parent)
-		return (NULL);
-
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+/*node->parent NULL = node is root/n->p->p NULL = parent of node is root*/
+if (!node || !node->parent || !node->parent->parent)
+	return (NULL);
+/*if node->parent is left child of his grand-parent*/
+if (node->parent->parent->left == node->parent)
+	return (node->parent->parent->right); /*uncle would be right child of gp*/
+return (node->parent->parent->left); /*if not uncle is left child of gp*/
 }
