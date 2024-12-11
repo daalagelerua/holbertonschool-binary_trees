aa5f5abad92231f6ef58eb80c37700b2bb3b8670
Coco29dev <10059@holbertonstudents.com>
2024-12-11 08:15:02 +0000

    betty task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,10 +13,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 binary_tree_t *new_node;
 /** verification si parent est null */
 	if (parent == NULL)
-return (NULL);
+	return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-return (NULL);
+	return (NULL);
 /** allocation memoire et verification de l'allocation */
 	new_node->n = value;
 	new_node->parent = parent;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - insert right-child node
+ * @parent: a pointer to the parent node
+ * @value: the value to be save in the new node
+ * Return: the new node
+ */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
 	if (parent == NULL)
-return (NULL);
+	return (NULL);
 /** verification si parent est null */
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-return (NULL);
+	return (NULL);
 /** allocation memoire et verification de l'allocation */
 	new_node->n = value;
 	new_node->parent = parent;
